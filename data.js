da428176a4dc7d2283b8e23688a8bb5f542e0018
thewhodidthis <thewhodidthis@fastmail.com>
2022-08-06 11:02:57 +0100

    js/data: strip object key quotes

--- data.js
+++ data.js
@@ -1,107 +1,107 @@
 export const project = {
-  "id": "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d",
-  "implementation": "Standard",
-  "projectId": "9",
-  "createdAtTimestamp": "1658501985",
-  "editionSize": "99",
-  "totalMinted": "1",
-  "name": "DoTs",
-  "symbol": "DOTS",
-  "creator": {
-    "id": "0x5712869b7c8ca52b00af7f6d924d4c841781ccd4",
+  id: "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d",
+  implementation: "Standard",
+  projectId: "9",
+  createdAtTimestamp: "1658501985",
+  editionSize: "99",
+  totalMinted: "1",
+  name: "DoTs",
+  symbol: "DOTS",
+  creator: {
+    id: "0x5712869b7c8ca52b00af7f6d924d4c841781ccd4",
   },
-  "editions": [
+  editions: [
     {
-      "burnedAtBlockNumber": null,
-      "burnedAtTimeStamp": null,
-      "createdAtBlockNumber": "27287388",
-      "createdAtTimestamp": "1658502702",
-      "createdAtTransactionHash": "0xba920701846515586558b6ebabe21151ce671b829c38aa9d5089396d8f2ee7cd",
-      "number": "1",
-      "id": "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d-1",
-      "owner": {
-        "id": "0x5f0009f23251feb0f58c8e3aab8e096af16faecd",
+      burnedAtBlockNumber: null,
+      burnedAtTimeStamp: null,
+      createdAtBlockNumber: "27287388",
+      createdAtTimestamp: "1658502702",
+      createdAtTransactionHash: "0xba920701846515586558b6ebabe21151ce671b829c38aa9d5089396d8f2ee7cd",
+      number: "1",
+      id: "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d-1",
+      owner: {
+        id: "0x5f0009f23251feb0f58c8e3aab8e096af16faecd",
       },
-      "prevOwner": {
-        "id": "0x0000000000000000000000000000000000000000",
+      prevOwner: {
+        id: "0x0000000000000000000000000000000000000000",
       },
-      "seed": null,
-      "uri":
+      seed: null,
+      uri:
         "data:application/json;base64,eyJuYW1lIjogIkRvVHMgMS85OSIsICJkZXNjcmlwdGlvbiI6ICJEZW1vIGFydHdvcmsiLCAiaW1hZ2UiOiAiaHR0cHM6Ly9hcndlYXZlLm5ldC9pbUxueXZHZzNaeUgtVzZnYlgyVk5yQkNjV3ZBSlQycERxNXJ2U0ZUeVk0L3RodW1ibmFpbC5wbmc/aWQ9MSIsICJhbmltYXRpb25fdXJsIjogImh0dHBzOi8vYXJ3ZWF2ZS5uZXQvaW1Mbnl2R2czWnlILVc2Z2JYMlZOckJDY1d2QUpUMnBEcTVydlNGVHlZND9pZD0xJmFkZHJlc3M9MHg4ZTdiZGNhODkxOThkNmQ4OWJiNGZjN2M5NDlkOGEyYzBiOWVlNThkIiwgIm1lZGlhX3ZlcnNpb24iOiAiMS4yLjMiLCAicGF0Y2hfbm90ZXMiOiAiIiwgInByb3BlcnRpZXMiOiB7Im51bWJlciI6IDEsICJuYW1lIjogIkRvVHMifX0=",
     },
   ],
-  "description": "Demo artwork",
-  "royaltyBPS": "10000",
-  "lastAddedVersion": {
-    "image": {
-      "url": "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4/thumbnail.png",
-      "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  description: "Demo artwork",
+  royaltyBPS: "10000",
+  lastAddedVersion: {
+    image: {
+      url: "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4/thumbnail.png",
+      hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
     },
-    "animation": {
-      "url": "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4",
-      "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+    animation: {
+      url: "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4",
+      hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
     },
   },
-  "versions": [
+  versions: [
     {
-      "id": "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d-1.2.3",
-      "label": "1.2.3",
-      "createdAtTimestamp": "1658501985",
-      "animation": {
-        "url": "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4",
-        "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+      id: "0x8e7bdca89198d6d89bb4fc7c949d8a2c0b9ee58d-1.2.3",
+      label: "1.2.3",
+      createdAtTimestamp: "1658501985",
+      animation: {
+        url: "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4",
+        hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
       },
-      "image": {
-        "url": "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4/thumbnail.png",
-        "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+      image: {
+        url: "https://arweave.net/imLnyvGg3ZyH-W6gbX2VNrBCcWvAJT2pDq5rvSFTyY4/thumbnail.png",
+        hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
       },
-      "patchNotes": {
-        "url": "",
-        "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+      patchNotes: {
+        url: "",
+        hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
       },
     },
   ],
-  "dutchAuctionDrops": [
+  dutchAuctionDrops: [
     {
-      "id": "5",
-      "startPrice": "100000000000000000",
-      "startTimestamp": "1659452371",
-      "status": "Active",
-      "endPrice": "0",
-      "endTimestamp": "1658504160",
-      "approved": true,
-      "duration": "20000000000000000",
-      "numberOfPriceDrops": 2000,
-      "creator": {
-        "id": "0x5712869b7c8ca52b00af7f6d924d4c841781ccd4",
+      id: "5",
+      startPrice: "100000000000000000",
+      startTimestamp: "1659452371",
+      status: "Active",
+      endPrice: "0",
+      endTimestamp: "1658504160",
+      approved: true,
+      duration: "20000000000000000",
+      numberOfPriceDrops: 2000,
+      creator: {
+        id: "0x5712869b7c8ca52b00af7f6d924d4c841781ccd4",
       },
-      "curator": {
-        "id": "0x0000000000000000000000000000000000000000",
+      curator: {
+        id: "0x0000000000000000000000000000000000000000",
       },
-      "project": {
-        "editionSize": "99",
+      project: {
+        editionSize: "99",
       },
-      "auctionCurrency": {
-        "id": "0xfe4f5145f6e09952a5ba9e956ed0c25e3fa4c7f1",
-        "decimals": 18,
-        "symbol": "DERC20",
-        "name": "Dummy ERC20",
+      auctionCurrency: {
+        id: "0xfe4f5145f6e09952a5ba9e956ed0c25e3fa4c7f1",
+        decimals: 18,
+        symbol: "DERC20",
+        name: "Dummy ERC20",
       },
-      "previousPurchases": [
+      previousPurchases: [
         {
-          "amount": "100000000000000000",
-          "createdAtTimestamp": "1658502702",
-          "currency": {
-            "decimals": 18,
-            "name": "Dummy ERC20",
-            "symbol": "DERC20",
+          amount: "100000000000000000",
+          createdAtTimestamp: "1658502702",
+          currency: {
+            decimals: 18,
+            name: "Dummy ERC20",
+            symbol: "DERC20",
           },
-          "collector": {
-            "id": "0x5f0009f23251feb0f58c8e3aab8e096af16faecd",
+          collector: {
+            id: "0x5f0009f23251feb0f58c8e3aab8e096af16faecd",
           },
-          "edition": {
-            "number": "1",
-            "seed": null,
+          edition: {
+            number: "1",
+            seed: null,
           },
         },
       ],
